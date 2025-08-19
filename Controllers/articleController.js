7aaec3d9977c541a9d3337a85b8a39b7b1f18e54
Srujan <Srujan2004ab@gmail.com>
2025-08-19 20:47:35 +0530

    Article CRUD Opertion Controller Updated

---
+++ Controllers/articleController.js
@@ -0,0 +1,47 @@
+const articleModel = require('../Model/articleModel')
+
+const ListArticles = async(req,res)=>{
+   try {
+    const Article = await articleModel.find();
+    return res.json({Article})
+   } catch (error) {
+    console.log(error); 
+   }}
+
+   const ListSingleArticle = async(req,res)=>{
+   try {
+    const Article = await articleModel.findById(req.params.articleId,req.body)
+    return res.json({Article});
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+const CreateArticle = async(req,res)=>{
+  try {
+    const Article = await articleModel.create(req.body);
+    return res.json({Article});
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+const UpdateArticle = async(req,res)=>{
+   try {
+    const Article = await articleModel.findByIdAndUpdate(req.params.articleId,req.body,{new:true});
+    return res.json({Article})
+   } catch (error) {
+    console.log(error);
+   }
+}
+
+const DeleteArticle = async(req,res)=>{
+try {
+    const Article = await articleModel.findByIdAndDelete(req.params.articleId,req.body)
+    return res.json({Article})
+} catch (error) {
+    console.log(error);  
+}
+}
+
+   module.exports = {ListArticles,ListSingleArticle,CreateArticle,UpdateArticle,DeleteArticle}
--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -1,52 +1,20 @@
 const express = require('express')
+const { ListArticles, ListSingleArticle, CreateArticle, UpdateArticle, DeleteArticle } = require('../Controllers/articleController')
 const router = express.Router()
-const articleModel = require('../Model/articleModel')
 
 //List All Articles
-router.get('/',async(req,res)=>{
-   try {
-    const Article = await articleModel.find();
-    return res.json({Article})
-   } catch (error) {
-    console.log(error); 
-   }})
+router.get('/',ListArticles)
+
 
 //List Single Article
-router.get('/:articleId',async(req,res)=>{
-   try {
-    const Article = await articleModel.findById(req.params.articleId,req.body)
-    return res.json({Article});
-   } catch (error) {
-    console.log(error);
-   }
-})
+router.get('/:articleId',ListSingleArticle)
 
-router.post('/',async(req,res)=>{
-  try {
-    const Article = await articleModel.create(req.body);
-    return res.json({Article});
-  } catch (error) {
-    console.log(error);
-  }
-})
+//Create Article
+router.post('/',CreateArticle)
 
 
-router.put('/:articleId',async(req,res)=>{
-   try {
-    const Article = await articleModel.findByIdAndUpdate(req.params.articleId,req.body,{new:true});
-    return res.json({Article})
-   } catch (error) {
-    console.log(error);
-   }
-})
+router.put('/:articleId',UpdateArticle)
 
-router.delete('/:articleId',async(req,res)=>{
-try {
-    const Article = await articleModel.findByIdAndDelete(req.params.articleId,req.body)
-    return res.json({Article})
-} catch (error) {
-    console.log(error);  
-}
-})
+router.delete('/:articleId',DeleteArticle)
 
 module.exports = router;
